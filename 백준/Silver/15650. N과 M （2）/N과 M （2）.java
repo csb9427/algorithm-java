4fe6ae9a378897250c5bc95f82ca64778b8fb266
Choi seungbin <80771842+csb9427@users.noreply.github.com>
2024-02-22 12:53:40 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 12320 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,52 +1,43 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+
 
 /*
- * 재귀함수로 풀어야 한다.
- * 자연수 N은 범위이고 M은 수열중에 몇개를 고를 지 정할 수 있다.
- * 고른 수열은 오름차순이어야 하며
- * 수열간의 중복이 없다.
  * 
- * 
- * 입력을 받는다.
- * 재귀함수로 넘긴다음 값의 개수가 다 골라졌으면 출력한다. 
- * 다만 값의 크기는 반드시 값보다 커야한다.
  */
 
-
 public class Main {
-	static int N;
-	static int R;
-	
-	static int[] picked; // 뽑은 요소 정보
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		R = sc.nextInt();
-		
-		picked = new int[R];
-		permu(0, 1);
-	}
-	/*
-	 *  숫자 한 개를 뽑고 다음 자리의 숫자는 재귀함수 호출로 맡김
-	 */
-	private static void permu(int idx, int start) {
-		
-		if(idx==R) {
-			for(int i=0; i<R; i++) {
-				System.out.printf("%d ", picked[i]);
-			}
-			System.out.println();
-			return;
-		}
-		
-		for(int i=start; i<=N; i++) {
-			picked[idx] = i; // 요소 뽑음
-			permu(idx+1, i+1); // 다음 요소 뽑기는 재귀에 맡긴다.
-			
-		}
-	}
 
+    static int N, M;
+    static int[] picked;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        picked = new int[M];
+        answer(0, 1);
+   
+    }
+
+    static void answer(int start, int end) {
+    	
+    	if(start==M) {
+    		for(int i=0; i<M; i++) System.out.printf("%d ", picked[i]);
+    		System.out.println();
+    		return;
+    	}
+    	
+        for(int i=end; i<=N; i++) {
+        	picked[start] = i;
+        	answer(start+1, i+1);
+        }
+    }
 }
