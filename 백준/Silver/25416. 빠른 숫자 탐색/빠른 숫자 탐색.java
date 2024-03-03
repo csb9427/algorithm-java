43361b2cd11fbe303fe94e635a00a151d5ea37cd
Choi seungbin <80771842+csb9427@users.noreply.github.com>
2024-03-03 12:07:12 +0900

    [Silver II] Title: 빠른 숫자 탐색, Time: 80 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Silver/25416. 빠른 숫자 탐색/빠른 숫자 탐색.java
+++ 백준/Silver/25416. 빠른 숫자 탐색/빠른 숫자 탐색.java
@@ -10,8 +10,9 @@ public class Main {
     static int[] dr = {1, -1, 0, 0};
     static int[] dc = {0 ,0, -1, 1};
 
-    static int[][] board = new int[5][5];
+    static int[][] map = new int[5][5];
     static boolean[][] visited = new boolean[5][5];
+    static int r, c;
 
     public static void main(String[] args) throws IOException {
 
@@ -20,15 +21,19 @@ public class Main {
         for(int i = 0; i < 5; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < 5; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j] == 1) {
+                    r = i;
+                    c = j;
+                }
             }
         }
         st = new StringTokenizer(br.readLine());
         int startR = Integer.parseInt(st.nextToken());
         int startC = Integer.parseInt(st.nextToken());
 
-        int result = BFS(startR, startC);
-        System.out.println(result);
+        int count = BFS(startR, startC);
+        System.out.println(count);
     }
 
     public static int BFS(int startR, int startC) {
@@ -42,29 +47,21 @@ public class Main {
             int currentC = current[1];
             int steps = current[2];
 
-            // 목적지에 도착한 경우 최단 거리 반환
-            if (board[currentR][currentC] == 1) {
+            if (currentR == r && currentC == c) {
                 return steps;
             }
 
-            // 네 방향으로 이동
             for (int i = 0; i < 4; i++) {
                 int newR = currentR + dr[i];
                 int newC = currentC + dc[i];
 
-                // 이동한 지점이 유효하고 방문하지 않았으며, 갈 수 있는 지점일 경우
-                if (isValid(newR, newC) && !visited[newR][newC] && board[newR][newC] != -1) {
+                
+                if (newR >= 0 && newR < 5 && newC >= 0 && newC < 5 && !visited[newR][newC] && map[newR][newC] != -1) {
                     queue.add(new int[]{newR, newC, steps + 1});
                     visited[newR][newC] = true;
                 }
             }
         }
-        // 목적지에 도착하지 못한 경우 -1 반환
         return -1;
-    }
-
-    // 주어진 좌표가 유효한 좌표인지 확인하는 메서드
-    public static boolean isValid(int row, int col) {
-        return row >= 0 && row < 5 && col >= 0 && col < 5;
     }
 }
